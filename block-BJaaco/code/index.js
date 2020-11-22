639865caaadaf3c0a25334c56fd1d8e117644869
jvegiraju <raju.vegiraju18@gmail.com>
2020-11-22 02:12:05 -0500

    Assignment Level 2

--- block-BJaaco/code/index.js
+++ block-BJaaco/code/index.js
@@ -3,29 +3,68 @@ let numbers = [6, 8, 10, 12, 43, 56, 98];
 let userIds = [1230, 234, 1278, 984, 763, 900];
 
 // 1. Add all the values of numbers and userIds array into the new newly created array named `collection`
+let collection = [];
+function push(arr, collection) {
+  for (i = 0; i < arr.length; i++) {
+    collection.push(arr[i]);
+  }
+}
+
+push(numbers, collection);
+push(userIds, collection);
+console.log(collection);
 
 // 2. Add all the even numbers from both arrays numbers and userIds into a newly created array named `evenCollection`
 
+let evenCollection = [];
+function push(arr, evenCollection) {
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evenCollection.push(arr[i]);
+    }
+  }
+}
+
+push(numbers, evenCollection);
+push(userIds, evenCollection);
+console.log(evenCollection);
+
 // 3. Add all the odd numbers from both arrays numbers and userIds into a newly created array named `oddCollection`
+let oddCollection = [];
+function push(arr, oddCollection) {
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      oddCollection.push(arr[i]);
+    }
+  }
+}
+
+push(numbers, oddCollection);
+push(userIds, oddCollection);
+console.log(oddCollection);
 
 /*
   @param means parameter
 
- 4. Write a function named times which accets two parameter and return an array. 
+ 4. Write a function named times which accets two parameter and return an array.
 
   @param times (number)
   @param character (string)
   @return array
 
-  Example: 
+  Example:
     times(5, 'c'); // ['c', 'c', 'c', 'c', 'c']
     times(2, 'a'); // ['a', 'a']
     times(0); // []
     times(5); // ['test', 'test', 'test', 'test', 'test']
 */
 
-function times() {
-  // Your code
+function times(times = 0, character = "test") {
+  let result = [];
+  for (i = 1; i <= times; i++) {
+    result.push(character);
+  }
+  return result;
 }
 
 // Uncomment the code below and test the output
@@ -37,19 +76,23 @@ function times() {
 
 /*
 
- 5. Write a function named revert which reverts the element of the input array. 
+ 5. Write a function named revert which reverts the element of the input array.
 
   @param arr (array)
   @return array
 
-  Example: 
+  Example:
     revert([1, 2, 3, 4]); // [4, 3, 2, 1]
     revert(['a', 'd', 'c', 'b']); // ['b', 'c', 'd', 'a']
     revert(['Ryan', 'John', 'Bran']); //['Bran', 'John', 'Ryan']
 */
 
-function revert() {
-  // your code
+function revert(arr) {
+  let result = [];
+  for (i = arr.length - 1; i >= 0; i--) {
+    result.push(arr[i]);
+  }
+  return result;
 }
 
 // Uncomment the code below and test the output
@@ -59,19 +102,30 @@ function revert() {
 
 /*
 
- 6. Write a function named clear which remove all of these values (false, undefined, empty string, 0, null) and returns a new array  
+ 6. Write a function named clear which remove all of these values (false, undefined, empty string, 0, null) and returns a new array
 
   @param arr (array)
   @return array
 
-  Example: 
+  Example:
     clear([1, 2, 3, 4, '', 0, null, undefined]); // [4, 3, 2, 1]
     clear(['a', undefined, 'd', 0,  'c', 'b']); // ['b', 'c', 'd', 'a']
     clear(['Ryan', null, 0,  'John', 'Bran']); //['Bran', 'John', 'Ryan']
 */
 
-function clear() {
-  // your code
+function clear(arr) {
+  let result = [];
+  for (i = arr.length - 1; i >= 0; i--) {
+    if (
+      arr[i] !== false &&
+      arr[i] !== undefined &&
+      arr[i] !== "" &&
+      arr[i] !== 0 &&
+      arr[i] !== null
+    )
+      result.push(arr[i]);
+  }
+  return result;
 }
 
 // Uncomment the code below and test the output
@@ -87,14 +141,18 @@ function clear() {
   @param arr (array)
   @return object
 
-  Example: 
+  Example:
     arrayToObj([1, 2, 3, 4]); // {0: 1, 1: 2, 2: 3, 3: 4}
     arrayToObj(['a', undefined, 'd']); // {0: 'a', 1: undefined, 2: 'd'}
     arrayToObj(['Ryan', 'John']); // {0: 'Ryan', 1: 'John'}
 */
 
-function arrayToObj() {
-  // your code
+function arrayToObj(arr) {
+  let obj = {};
+  for (i = 0; i < arr.length; i++) {
+    obj[i] = arr[i];
+  }
+  return obj;
 }
 
 // Uncomment the code below and test the output
